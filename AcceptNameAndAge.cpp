2be6fe085f6a4b7fe9e3bfbcd2b49dcef222562d
Adarsh Soni <soniadarsh487@gmail.com>
2025-07-17 05:57:10 +0530

    Add AcceptNameAndAge.cpp to implement a program that greets the user with their name and age

---
+++ AcceptNameAndAge.cpp
@@ -0,0 +1,19 @@
+//  ------------------------------             Before jumping into the code, please try it first yourself   -----------------------------
+#include <iostream>
+using namespace std;
+int main()
+{
+    string name;
+    cout << "Enter your name : ";
+    cin >> name;
+    int age;
+    cout << "Enter your age:  ";
+    cin >> age;
+
+    cout << "Hello " << name << " your age is " << age << endl;
+    return 0;
+}
+
+/*
+Explaination:  We first of all declare two variables name and age as your name of string type and second age of int type then we can directly print the result by cout << "Hello " << name << " your age is " << age << endl;
+*/
