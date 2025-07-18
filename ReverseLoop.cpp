57226f28fbc4ca218709dda91f7c884a3b111318
Adarsh Soni <soniadarsh487@gmail.com>
2025-07-18 06:01:45 +0530

    Add ReverseLoop.cpp to implement a program that prints numbers in reverse order from a user-defined limit.

---
+++ ReverseLoop.cpp
@@ -0,0 +1,24 @@
+//  ------------------------------
+//        REVERSE FOR LOOP FROM N TO 1
+//  ------------------------------
+//  Before jumping into the code, please try it yourself first :)
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+
+    cout << "Entern n : ";
+
+    cin >> n;
+
+    for (int i = n; i > 0; i -= 1)
+    {
+        cout << i << " ";
+    }
+    return 0;
+}
+
+
+// This is a just reverse loop I don't think you will need to be explained.
