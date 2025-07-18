643e7e312541c361293be417c452593ca0819d8e
Adarsh Soni <soniadarsh487@gmail.com>
2025-07-19 03:53:18 +0530

    Added 3 approaches to find all factors of a number (Brute Force, Half Optimization, sqrt(n) Optimization)

--- FactorOfN.cpp
+++ FactorOfN.cpp
@@ -1,5 +1,6 @@
 //  ------------------------------             Before jumping into the code, please try it yourself first  -----------------------------
 
+#include <cmath>
 #include <iostream>
 using namespace std;
 
@@ -36,6 +37,25 @@ int main()
     // At the end, we include the number itself since it always divides itself and above loop is printing till n/2 not till n
     cout << n << " ";
 
+    // Approach 3
+
+    /* We can also loop throught sqrt(n) because think about it that for any 64 the factors is in pair (1*64) (2*32) ans so on and
+    if i is a factor of n so we can say than n/i is also its factor for e.g. n = 64 if 8 is its factor then 64/8 is also its factor */
+
+    for (int i = 1; i <= sqrt(n); i += 1)
+    {
+        if (n % i == 0)
+        {
+            cout << i << " ";
+
+            // Avoid printing the same factor twice when i * i == n because for e.g. n = 64 the factors will be (8*8) which is repeating so we checked that condition here
+            if (i != n / i)
+            {
+                cout << n / i << " ";
+            }
+        }
+    }
+
     return 0;
 }
 
@@ -52,4 +72,6 @@ This program prints all the factors (or divisors) of a given number `n`.
    For example: if n = 9, then n / 2 = 4, and the loop runs from 1 to 4 (which is sufficient).
 
 👉 Finally, we manually print `n` itself, as every number is divisible by itself.
-*/
+
+
+✔ In **Approach 3**, we use a loop from 1 to sqrt(n) and check if `n % i == 0`. If it is true, then `i` is a factor and because the factors are like let's say n = 64 then 1*64, 2*32, 4*16 8*8 so we checked if i != n/i then only we print it correctly prints only unique factors of n*/
