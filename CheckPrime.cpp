8a14996af33e96a105a3e294580c3900b8daadff
Adarsh Soni <soniadarsh487@gmail.com>
2025-07-21 08:57:26 +0530

    Added one more approach t solve this problem which is of O(n/2) tc

--- CheckPrime.cpp
+++ CheckPrime.cpp
@@ -16,43 +16,71 @@ int main()
 
     int factorsOfN = 0;
 
-    for (int i = 1; i <= n; i += 1)
-    {
-        if (n % i == 0)
-        {
-            factorsOfN += 1;
-        }
-    }
+    // for (int i = 1; i <= n; i += 1)
+    // {
+    //     if (n % i == 0)
+    //     {
+    //         factorsOfN += 1;
+    //     }
+    // }
 
-    if (factorsOfN == 2)
-    {
-        cout << "Given number is a prime";
-        return 0;
-    }
-    else
-    {
-        cout << "Given number is not a prime";
-        return 0;
-    }
+    // if (factorsOfN == 2)
+    // {
+    //     cout << "Given number is a prime";
+    //     return 0;
+    // }
+    // else
+    // {
+    //     cout << "Given number is not a prime";
+    //     return 0;
+    // }
 
-    // ---------------------- Approach 2: Optimized - Check only up to √n ----------------------
+    // // ---------------------- Approach 2: Better ne - Check only up to n/2 ----------------------
 
     if (n <= 1)
     {
-        cout << n << " is not a prime number";
+        cout << n << " is  a prime number";
         return 0;
     }
 
-    for (int i = 2; i <= sqrt(n); i++)
+    bool isPrime = true;
+
+    for (int i = 2; i <= n / 2; i++)
     {
         if (n % i == 0)
         {
-            cout << n << " is not a prime number";
-            return 0;
+            isPrime = false;
+            break;
         }
     }
 
-    cout << n << " is a prime number";
+    if (isPrime)
+    {
+        cout << "Prime Number";
+    }
+    else
+    {
+        cout << "Not a prime number";
+    }
+
+    // // ---------------------- Approach 3: Optimized - Check only up to √n ----------------------
+
+    // if (n <= 1)
+    // {
+    //     cout << n << " is  a prime number";
+    //     return 0;
+    // }
+
+    // for (int i = 2; i <= sqrt(n); i++)
+    // {
+    //     if (n % i == 0)
+    //     {
+    //         cout << n << " is not a prime number";
+    //         return 0;
+    //     }
+    // }
+
+    // cout << n << " is a prime number";
 
     return 0;
 }
@@ -69,12 +97,30 @@ EXPLANATION:
 - If the total count of such divisors is exactly 2, then it's a prime number.
 - Time Complexity: O(n)
 
-🟢 Approach 2: Optimized using Square Root
+
+🟢 Approach 2: Better looping upton n/2
+Why i <= n/2 works:
+Any number n cannot have a factor greater than n/2 other than n itself.
+
+For example:
+
+For n = 8, potential factors other than 1 and 8 are 2, 4 (both ≤ 4).
+
+So if no number between 2 and n/2 divides n, then n must be prime.
+
+- So instead of checking till n, we check only up to n/2, which is slight faster.
+- If any number in this range divides `n`, then it's not prime.
+- Else, it's a prime.
+- Time Complexity: O(n/2)
+
+🟢 Approach 3: Optimized using Square Root
 - A number is NOT prime if it is divisible by any number from 2 to √n.
 - So instead of checking till n, we check only up to sqrt(n), which is much faster.
 - If any number in this range divides `n`, then it's not prime.
 - Else, it's a prime.
 - Time Complexity: O(√n)
+
+
 
 📌 NOTE: If `n <= 1`, it is not a prime number by definition.
 
